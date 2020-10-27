d6f3236353be810b59cd828a7b1e7be9e06aa51d
Teyber <boucettaghali@gmail.com>
2020-10-27 04:20:20 +0100

    Constructor of DBManager private

--- Code/include/DBManager.hpp
+++ Code/include/DBManager.hpp
@@ -19,13 +19,13 @@ private:
 	// Fonctions à stocker dans le dictionnaire de fonctions des commandes
 	static void createRelation(std::string args);
 
+	DBManager();
 public:
 	// Suppresion du constructeur de copie (impossible de construire un DBManager en utilisant une autre instance de DBManager)
 	DBManager(const DBManager&) = delete;
 	// Suppresion de l'opérateur de copie (impossibilité de faire DBManager a = b, où b est un DBManager)
 	DBManager& operator=(const DBManager&) = delete;
 
-	DBManager(/* args */);
 	~DBManager();
 	static DBManager* getInstance();
 	void init();
